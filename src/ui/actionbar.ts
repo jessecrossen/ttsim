e03f0764025c1994b8218467650075b7258aeb55
Jesse Crossen <jesse.crossen@gmail.com>
2019-02-06 09:53:48 -0500

    Stop using old-style wheel events.

--- src/ui/actionbar.ts
+++ src/ui/actionbar.ts
@@ -65,8 +65,9 @@ export class Actionbar extends ButtonBar {
     this.updateToggled();
     // zoom on wheel events
     document.addEventListener('wheel', (e:WheelEvent) => {
-      if (e.wheelDelta > 0) this.zoomIn();
-      else if (e.wheelDelta < 0) this.zoomOut();
+      console.log(e);
+      if ((e.deltaY < 0) || (e.deltaX < 0) || (e.deltaZ < 0)) this.zoomIn();
+      else if ((e.deltaY > 0) || (e.deltaX > 0) || (e.deltaZ > 0)) this.zoomOut();
       e.preventDefault();
     });
   }
